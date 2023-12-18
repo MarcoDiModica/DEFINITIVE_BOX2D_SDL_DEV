ced909eb01edae8daa74cfe7c13f5b346ad44a86
Marco Di Modica Elias <marcodimodica@protonmail.com>
2023-12-18 20:14:40 +0100

    as

---
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Source/Enemy2.h
@@ -0,0 +1,74 @@
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+#include "Animation.h"
+#include "Box2D/Box2D/Box2D.h"
+
+struct SDL_Texture;
+
+class Enemy : public Entity
+{
+public:
+
+	Enemy();
+
+	virtual ~Enemy();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+	void Death();
+
+	void SetPath(b2Vec2 path);
+
+	bool LoadState(pugi::xml_node node, int num);
+
+	bool SaveState(pugi::xml_node node, int num);
+public:
+	const char* texturePath;
+	const char* deathSFXPath;
+	uint deathSFX;
+	bool audiohasplayed = false;
+	SDL_Texture* texture = nullptr;
+	SDL_Texture* pathTexture = nullptr;
+	Animation* currentAnimation = nullptr;
+
+	SDL_RendererFlip lastDirection = SDL_FLIP_NONE;
+	SDL_RendererFlip flipHorizontal = lastDirection;
+	SDL_RendererFlip flipVertical = SDL_FLIP_NONE;
+
+	Animation idleAnim;
+	Animation runAnim;
+	Animation JumpAnim;
+	Animation DeathAnim;
+
+	
+	bool death = false;
+	bool debug = false;
+	bool isMoving = false;
+	bool isTouchingGround = true;
+	int width;
+	int height;
+
+	
+
+	float gravityScale = 1.0f;
+	int initX;
+	int initY;
+
+	b2Vec2 path1;
+	int currentTargetIndex;
+	int enemySpeed = 0.3f;
+};
+
+#endif // __ENEMY_H__
